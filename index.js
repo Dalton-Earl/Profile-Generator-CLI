0765aa10fe30f06ccafa76089314f2ddbf90a5c4
Dalto <dalton.earl@outlook.com>
2020-04-18 10:09:34 -0600

    the inquirer prompt is working

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const questions = [
-  
-];
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateHTML = require("./generateHTML");
+
+
+function promtUser(){
+    const questions = [
+        {
+            type: "input",
+            name: "GitHub Username",
+            message: "Enter the github profile you would like a PDF of"
+        },
+        {
+            type: "list",
+            name: "color",
+            message:"what is your favorite Color?",
+            choices: [
+                'green',
+                'blue',
+                'pink',
+                'red'
+            ]
+        }
+      ];
+    return inquirer.prompt(questions);
+
+}
 
 function writeToFile(fileName, data) {
  
 }
 
-function init() {
+async function init() {
+    console.log("running init")
+    try{
+        const answers = await promtUser();
+
+        const html = generateHTML(answers);
+    }catch(err){
+        console.log(err);
+    }
+}
 
 init();
